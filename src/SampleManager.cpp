bf7f55f784ce1882a1eed00a20fdb27b45a94744
Faidide <quentin.faidide@gmail.com>
2022-11-26 03:40:21 +0100

    add base SampleManager skeleton code

--- src/SampleManager.cpp
+++ src/SampleManager.cpp
@@ -1,15 +1,80 @@
 #include "SampleManager.h"
 #include <iostream>
 
-SampleManager::SampleManager() {}
+// initialize the SampleManager, as well as Thread and audio inherited
+// behaviours.
+SampleManager::SampleManager():
+    playCursor (0),
+    Thread ("Background Thread")
+{
+    // initialize format manager
+    formatManager.registerBasicFormats();
 
-SampleManager::~SampleManager() {}
+    // set 0 inputs and two outputs
+    // TODO: make this a cli arg
+    setAudioChannels (0, 2);
+
+    // start the background thread that makes malloc/frees
+    startThread();
+}
+
+SampleManager::~SampleManager() {
+    // stop thread with a 4sec timeout to kill it
+    stopThread (4000);
+    // shutdown the audio
+    shutdownAudio();
+}
 
 bool SampleManager::filePathsValid(const juce::StringArray& files) {
+    // TODO: implement this
     return true;
 }
 
 int SampleManager::addSample(juce::String filePath, int64_t frameIndex) {
-    // for now, return -1
-    return -1;
+    // swap a file to load variable to avoid blocking audio thread for disk i/o
+    {
+        // get lock for scoped block
+        const juce::ScopedLock lock (pathMutex);
+        // swap the strings with the one to load
+        filePathToImport.swapWith (filePath);
+        // record the desired frame position to insert at
+        filePositionToImport = frameIndex;
+    }
+    // notify the thread so it's triggered
+    notify();
+    return 0;
+}
+
+void SampleManager::prepareToPlay(int a, double b) {
+    // TODO: find out how to use this properly
+}
+
+void SampleManager::releaseResources() {
+    // TODO: document myself better on when this is called
+    // TODO: get main lock
+    // TODO: invalidate pointers so they can be freed
+}
+
+void SampleManager::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) {
+    // TODO: get the block from MixerAudioSource ?
+}
+
+// background thread content for allocating stuff
+void SampleManager::run() {
+    // wait 500ms in a loop unless notify is called
+    while(! threadShouldExit()) {
+        // check for files to import
+        checkForFileToImport();
+        // check for buffers to free
+        checkForBuffersToFree();
+        wait(500);
+    }
+}
+
+void SampleManager::checkForBuffersToFree() {
+    // TODO
+}
+
+void SampleManager::checkForFileToImport() {
+    // TODO
 }
--- src/SampleManager.h
+++ src/SampleManager.h
@@ -1,23 +1,74 @@
 #ifndef DEF_SAMPLEMANAGER_HPP
 #define DEF_SAMPLEMANAGER_HPP
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module
-// headers directly. If you need to remain compatible with Projucer-generated
-// builds, and have called `juce_generate_juce_header(<thisTarget>)` in your
-// CMakeLists.txt, you could `#include <JuceHeader.h>` here instead, to make all
-// your module headers visible.
+/**
+  *
+
+  SampleManager will load, mix, process and play
+  the audio samples loaded with the interface.
+
+  */
+
 #include <juce_gui_extra/juce_gui_extra.h>
 
-#include "FileError.h"
+#include "Sample.h"
 
-class SampleManager {
+class SampleManager: public juce::AudioAppComponent,
+                     private juce::Thread {
  public:
   SampleManager();
   ~SampleManager();
 
-  // when called, add sample from file path with coordinates
+  // when called, add sample from file path with position
   int addSample(juce::String, int64_t);
   bool filePathsValid(const juce::StringArray&);
+  
+  // inherited from audio source
+  void getNextAudioBlock (const juce::AudioSourceChannelInfo&) override;
+  void prepareToPlay(int, double) override;
+  void releaseResources() override;
+
+private:
+
+  // file formats manager
+  juce::AudioFormatManager formatManager;
+
+  // play cursom position in audio frames
+  int64_t playCursor;
+  // list of Sample objects that retains references to their underlying ReferenceCountedBuffer or position ?
+  
+  // list of AudioSampleBuffer or ReferenceCountedBuffer that are playing a sample ?
+  // TODO: read again the code for this class
+  juce::ReferenceCountedArray<ReferenceCountedBuffer> buffers;
+
+  // list of IIRFilterAudioSource audio sources to apply on Audio Samples ?
+  // TODO: look up how to glue sources together.
+  //       Enventually create tracks as PositionableAudioSource or AudioSource
+  
+  // a MixerAudioSource class that will mix all currently playing tracks.
+  // TODO: check if we need to swap audio sources live with addInputSource.. or
+  //       if we can leave one per sample and just shift samples positions as needed
+
+  // used to manage background thread allocations
+  void checkForFileToImport();
+  void checkForBuffersToFree();
+
+  // mutex for buffer swapping
+  juce::SpinLock mutex;
+  
+  ReferenceCountedBuffer::Ptr currentBuffer;
+  
+  // mutex to swap the path
+  // TODO: read on CriticalSection locks and SpinLock
+  juce::CriticalSection pathMutex;
+
+  // path of the next file to import
+  juce::String filePathToImport;
+  // position to import file at
+  int64_t filePositionToImport;
+
+  // inherited from thread
+  void run() override;
 };
 
 #endif  // DEF_SAMPLEMANAGER_HPP
