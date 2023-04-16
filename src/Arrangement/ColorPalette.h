a5818433c27f6b5b3659c33079d0150997468cc1
Faidide <quentin.faidide@gmail.com>
2023-04-16 22:36:02 +0200

    feat: new color palette

--- src/Arrangement/ColorPalette.h
+++ src/Arrangement/ColorPalette.h
@@ -7,16 +7,10 @@
 
 // made from https://medialab.github.io/iwanthue/
 std::vector<juce::Colour> colourPalette = {
-    juce::Colour::fromString("ff5e40d0"), juce::Colour::fromString("ff5aa635"),
-    juce::Colour::fromString("ffb74bdf"), juce::Colour::fromString("ff489f61"),
-    juce::Colour::fromString("ffce4ab7"), juce::Colour::fromString("ff476827"),
-    juce::Colour::fromString("ff613c9f"), juce::Colour::fromString("ff959138"),
-    juce::Colour::fromString("ff7c76da"), juce::Colour::fromString("ffd28230"),
-    juce::Colour::fromString("ff5981bf"), juce::Colour::fromString("ffe04431"),
-    juce::Colour::fromString("ff3a9a85"), juce::Colour::fromString("ffd83f76"),
-    juce::Colour::fromString("ff8f6430"), juce::Colour::fromString("ff7c3774"),
-    juce::Colour::fromString("ffa24024"), juce::Colour::fromString("ffc172aa"),
-    juce::Colour::fromString("ffda766e"), juce::Colour::fromString("ff923948"),
-};
+    juce::Colour::fromString("ff6cbf73"), juce::Colour::fromString("ffd470e7"),
+    juce::Colour::fromString("ff80c239"), juce::Colour::fromString("ffe97bbd"),
+    juce::Colour::fromString("ff40c6b6"), juce::Colour::fromString("ffe09b2e"),
+    juce::Colour::fromString("ff7b98ea"), juce::Colour::fromString("ffc4a756"),
+    juce::Colour::fromString("ffb4a1e2"), juce::Colour::fromString("ffeb8071")};
 
 #endif  // DEF_COLOR_PALETTE_HPP
