b9a1e9dfaff9a68b6152819429c1d757711a087f
Faidide <quentin.faidide@gmail.com>
2023-05-14 13:54:57 +0200

    feat: enhance topbar layout

--- src/UserInterface/Topbar/TopbarRightArea.cpp
+++ src/UserInterface/Topbar/TopbarRightArea.cpp
@@ -13,19 +13,16 @@ TopbarRightArea::TopbarRightArea(ActivityManager &am) : colorPicker(am)
 void TopbarRightArea::paint(juce::Graphics &g)
 {
     auto constBounds = g.getClipBounds();
-    bounds = constBounds.reduced(TOPBAR_SECTIONS_INNER_MARGINS / 2, TOPBAR_SECTIONS_INNER_MARGINS).toFloat();
+    bounds = constBounds.toFloat();
 
+    // draw a separator to the left
     auto leftLine = juce::Line<float>(bounds.getTopLeft(), bounds.getBottomLeft());
-    auto rightLine = juce::Line<float>(bounds.getTopRight(), bounds.getBottomRight());
-
     g.setColour(COLOR_LABELS_BORDER.withAlpha(0.5f));
     g.drawLine(leftLine, 0.5);
-    g.drawLine(rightLine, 0.5);
 }
 
 void TopbarRightArea::resized()
 {
-    auto colorPickerBounds =
-        getLocalBounds().reduced(TOPBAR_SECTIONS_INNER_MARGINS * 2, 0).removeFromRight(COLORPICKER_WIDTH);
+    auto colorPickerBounds = getLocalBounds().removeFromRight(COLORPICKER_WIDTH);
     colorPicker.setBounds(colorPickerBounds);
 }
