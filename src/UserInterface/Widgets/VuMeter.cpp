064d1e195dc78188fc729800bb233002caf33590
Quentin <quentin.faidide@gmail.com>
2023-05-16 13:52:15 +0000

    add more drawing of vumeter

--- src/UserInterface/Widgets/VuMeter.cpp
+++ src/UserInterface/Widgets/VuMeter.cpp
@@ -10,34 +10,40 @@ void VuMeter::paint(juce::Graphics &g)
 {
     auto bounds = g.getClipBounds();
     
-    if (bounds.getWidth() < (VUMETER_WIDTH+(2*margins)) )
+    // abort if no space available
+    if (bounds.getWidth() < (VUMETER_WIDTH+(2*TOPBAR_SECTIONS_INNER_MARGINS)) )
     {
         return;
     }
 
+    // draw the top title
     juce::Colour bg = juce::Colours::transparentBlack;
     drawSection(g, bounds, title, bg);
 
-    // get widget area
+    // focus on the area without the title and margins
     auto boxesArea = bounds;
     boxesArea.removeFromTop(SECTION_TITLE_HEIGHT);
-    boxesArea.reduce(margins, margins);
+    boxesArea.reduce(TOPBAR_SECTIONS_INNER_MARGINS, TOPBAR_SECTIONS_INNER_MARGINS);
 
-    int emptySidesWidth = (bounds.getWidth()-VUMETER_WIDTH)/2;
-    
     // extract the bottom area where moving average values are show
     auto maxValArea = boxesArea.removeFromBottom(VUMETER_MAXVAL_HEIGHT);
-
+    // draw borders around it
     g.setColor(COLOR_TEXT_DARKER);
     g.drawRect(maxValArea.reduced(2));
     
+    // what's the size of the remaining side parts ?
+    int emptySidesWidth = (boxesArea.getWidth()-VUMETER_WIDTH)/2;
     // TODO: draw scales in those empty area
-    bounds.reduce(emptySidesWidth, 0);
+
+    // remove the side areas
+    boxesArea.reduce(emptySidesWidth, 0);
 
     // now our bounds perfectly fit the vumeter
     // area where we draw
+    g.setColor(COLOR_BACKGROUND);
+    g.fillRect(boxesArea);
     g.setColor(COLOR_TEXT_DARKER);
-    g.drawRect(bounds.reduced(2));
+    g.drawRect(boxesArea);
 }
 
 void VuMeter::mouseDrag(const juce::MouseEvent &event)
