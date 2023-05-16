0a487453490c4dff8666c640b43b2cde3a65080d
Quentin <quentin.faidide@gmail.com>
2023-05-16 15:27:01 +0000

    doc: add many comments

--- src/UserInterface/Widgets/VuMeter.h
+++ src/UserInterface/Widgets/VuMeter.h
@@ -12,21 +12,58 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
+/*
+Displays a stereo VuMeter horizontal vu meter with
+title and max values at the bottom.
+*/
 class VuMeter : public juce::Component
 {
   public:
+    /*
+    Initialize the vumeter with the given title.
+    */
     VuMeter(std::string);
+
+    /*
+    paint the vu meter
+    */
     void paint(juce::Graphics &g) override;
+
+    /*
+    Responds to mouse drag events.
+    */
     void mouseDrag(const juce::MouseEvent &event) override;
+
+    /*
+    sets the decibel values for left and right
+    */
     void setDbValue(float, float);
 
   private:
+
+    // the actual decibels values of left and righht channel
     float dbValueLeft, dbValueRight;
+
+    // the maximum values we've seen in a while
     float dbMaxLeft, dbMaxRight;
+
+    // the title of the vu meter
     std::string title;
 
+    /*
+    Return the area within the section at the desired vu meter width.
+    (No title and margins)
+    */
     void zoomToInnerSection(juce::Rectangle<int>);
+
+    /*
+    Paint the core visualizer part of the vu meter
+    */
     void paintCoreMeter(juce::Graphics &, juce::Rectangle<int>);
+
+    /*
+    Paint the max db values information below the visualizer.
+    */
     void paintBottomValues(juce::Graphics &, juce::Rectangle<int>);
 };
 
