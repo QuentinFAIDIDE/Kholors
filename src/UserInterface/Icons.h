6c1683549aaeb452694519bf41787f8d4ba86608
Faidide <quentin.faidide@gmail.com>
2023-06-19 22:12:34 +0200

    feat: add svg icons data

---
+++ src/UserInterface/Icons.h
@@ -0,0 +1,22 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace Icons
+{
+    extern const char*  loop_svg;
+    const int           loop_svgSize = 3075;
+
+    extern const char*  unloop_svg;
+    const int           unloop_svgSize = 3569;
+
+    extern const char*  play_svg;
+    const int           play_svgSize = 1748;
+
+    extern const char*  start_svg;
+    const int           start_svgSize = 2176;
+
+    extern const char*  pause_svg;
+    const int           pause_svgSize = 2144;
+
+}
