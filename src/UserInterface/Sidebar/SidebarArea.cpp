ac4e7f96ab583683fc020166e025510a5257769e
Faidide <quentin.faidide@gmail.com>
2023-09-28 17:05:50 +0200

    feat: remove notification system for future replacement

--- src/UserInterface/Sidebar/SidebarArea.cpp
+++ src/UserInterface/Sidebar/SidebarArea.cpp
@@ -6,15 +6,7 @@ SidebarArea::SidebarArea(ActivityManager &am)
     : playButton(am), stopButton(am), loopButton(am), colorPicker(am), trackProperties(am), sampleProperties(am),
       selectionGainVu("SELECTION", VUMETER_ID_SELECTED), masterGainVu("MASTER", VUMETER_ID_MASTER), activityManager(am)
 {
-
-    isHidden = true;
-    isAnimationRunning = false;
-    notifBaseX = 0;
-    notifBaseY = 0;
-    popupY = NOTIF_OUTTER_MARGINS;
-    popupX = NOTIF_WIDTH + (2 * NOTIF_OUTTER_MARGINS);
     setFramesPerSecond(NOTIF_ANIM_FPS);
-    animationNormalisingFactor = float(NOTIF_WIDTH + (2.0 * NOTIF_OUTTER_MARGINS));
 
     addAndMakeVisible(playButton);
     addAndMakeVisible(stopButton);
@@ -37,16 +29,9 @@ void SidebarArea::setDataSource(std::shared_ptr<MixbusDataSource> ds)
     trackProperties.setDataSource(ds);
 }
 
-bool SidebarArea::taskHandler(std::shared_ptr<Task> task)
+bool SidebarArea::taskHandler(std::shared_ptr<Task>)
 {
-    std::shared_ptr<NotificationTask> notif = std::dynamic_pointer_cast<NotificationTask>(task);
-
-    if (notif != nullptr && !notif->isCompleted() && !notif->hasFailed())
-    {
-        notifyMessage(notif->getMessage());
-        return true;
-    }
-
+    // NOTE: Used to pick up notifications, not anymore !
     return false;
 }
 
@@ -65,151 +50,6 @@ void SidebarArea::paint(juce::Graphics &g)
 
     g.setFont(DEFAULT_FONT_SIZE);
     g.drawText(projectName, projectTitleArea, juce::Justification::centredLeft, true);
-}
-
-void SidebarArea::paintOverChildren(juce::Graphics &g)
-{
-    // make sure the notification are filetered to remove old ones
-    trimNotifications();
-
-    // if we can't get a read lock, skip any action
-    if (!queueRwMutex.tryEnterRead())
-    {
-        return;
-    }
-
-    // TODO: do nothing whenever nothing changed
-    // and nothing requires anything to start or end.
-
-    // get the window width
-    bounds = g.getClipBounds();
-
-    // base location of the notification area
-    notifBaseX = bounds.getWidth() - 2 * NOTIF_OUTTER_MARGINS - NOTIF_WIDTH;
-
-    // get time of now to reuse it in the animation drawings parts
-    now = (int)juce::Time::getMillisecondCounter();
-
-    // if we are currently performing an animation
-    if (isAnimationRunning)
-    {
-        timeSinceAnimStart = now - animStartTime;
-
-        // the distance shift from the beginning of the anim
-        int distanceDiff = int(
-            // make it over 1 to use the ease in function
-            easeIn((NOTIF_ANIMATION_SPEED_PIXEL_MS * float(timeSinceAnimStart)) / animationNormalisingFactor)
-            // scale it back up after applying ease in function
-            * animationNormalisingFactor);
-
-        // if it's going out go left
-        if (isHidden == true)
-        {
-            popupX = NOTIF_OUTTER_MARGINS + distanceDiff;
-            // if it reached destination, stop it
-            if (popupX >= destinationX)
-            {
-                popupX = destinationX;
-                isAnimationRunning = false;
-            }
-
-            // if it's going in go right
-        }
-        else
-        {
-            // new position is the screen limit minus the notif movement toward
-            // center. we make the fade in faster than fade out for aesthetic reasons
-            popupX = NOTIF_WIDTH + (2 * NOTIF_OUTTER_MARGINS) - int(float(distanceDiff) * 4.0);
-            // if it reached destination, stop it
-            if (popupX <= destinationX)
-            {
-                popupX = destinationX;
-                isAnimationRunning = false;
-            }
-        }
-
-        // if we are not performing an animation
-    }
-    else
-    {
-        // if we need to fade out (queue empty and notif not hidden)
-        if (notifQueue.size() == 0 && !isHidden)
-        {
-            // trigger the animation to the notif place.
-            // destination is out if the screen
-            destinationX = NOTIF_WIDTH + (2 * NOTIF_OUTTER_MARGINS);
-            isAnimationRunning = true;
-            isHidden = true;
-            animStartTime = now;
-        }
-
-        // if we need to fade in (queue not empty and notif hidden)
-        else if (notifQueue.size() > 0 && isHidden)
-        {
-            // if so trigger the animation.
-            destinationX = NOTIF_OUTTER_MARGINS;
-            isAnimationRunning = true;
-            animStartTime = now;
-            isHidden = false;
-        }
-    }
-
-    // if the box is in bound, draw it
-    if (notifBaseX + popupX < bounds.getWidth())
-    {
-        g.setColour(COLOR_BACKGROUND);
-
-        // draw background box
-        // font size:
-        g.setFont(17); // TODO: Make it an app wide param
-        g.fillRoundedRectangle(notifBaseX + popupX, notifBaseY + popupY, NOTIF_WIDTH, NOTIF_HEIGHT,
-                               NOTIF_BORDER_RADIUS);
-        // draw text
-
-        g.setColour(COLOR_TEXT);
-        g.drawRoundedRectangle(notifBaseX + popupX, notifBaseY + popupY, NOTIF_WIDTH, NOTIF_HEIGHT, NOTIF_BORDER_RADIUS,
-                               1.0);
-
-        g.drawMultiLineText(lastNotification.message, notifBaseX + popupX + NOTIF_INNER_MARGINS,
-                            notifBaseY + popupY + NOTIF_INNER_MARGINS, NOTIF_WIDTH - NOTIF_INNER_MARGINS * 2,
-                            juce::Justification::left, 0.0f);
-    }
-
-    // free the lock
-    queueRwMutex.exitRead();
-}
-
-void SidebarArea::trimNotifications()
-{
-    // get current timestamp
-    uint32_t timestamp = juce::Time::getMillisecondCounter();
-    // get the lock for reading the queue
-    queueRwMutex.enterRead();
-    // iterate over queue elements if they exists
-    while (notifQueue.size() > 0)
-    {
-        // if it's outdated, remove it and continue
-        if (notifQueue.front().timestamp + NOTIF_TIMEOUT < timestamp)
-        {
-            // switch from a read to a write lock
-            queueRwMutex.exitRead();
-            queueRwMutex.enterWrite();
-            // do the queue editing
-            notifQueue.pop();
-            // switch from a write to a read lock
-            queueRwMutex.exitWrite();
-            queueRwMutex.enterRead();
-            continue;
-            // this branch is traversed if the oldest elem has not timed out
-        }
-        else
-        {
-            // this means other elemnts won't too so we break out of loop
-            break;
-        }
-    }
-    // free the lock for reading the queue
-    queueRwMutex.exitRead();
 }
 
 void SidebarArea::resized()
@@ -245,56 +85,6 @@ void SidebarArea::resized()
     mainArea.removeFromTop(SECTION_COMPONENTS_PADDING);
     sampleProperties.setBounds(mainArea.removeFromTop(sampleProperties.getIdealHeight()));
 };
-
-// must be called from messagemanager thread
-void SidebarArea::notifyMessage(const juce::String &msg)
-{
-    // get a read lock
-    queueRwMutex.enterRead();
-    // if the queue is full ignore the notification
-    if (notifQueue.size() > NOTIF_MAX_QUEUE_SIZE)
-    {
-        return;
-    }
-    // free read lock
-    queueRwMutex.exitRead();
-
-    // get current timestamp
-    uint32_t timestamp = juce::Time::getMillisecondCounter();
-    ;
-    // create the new notification
-    lastNotification.timestamp = timestamp;
-    lastNotification.message = msg;
-
-    // get the lock for writing to the queue
-    queueRwMutex.enterWrite();
-    // push the new notification to the queue
-    notifQueue.push(lastNotification);
-    // exit the writing lock
-    queueRwMutex.exitWrite();
-
-    std::cerr << "New notification at " << timestamp << ": " << msg << std::endl;
-
-    repaint();
-}
-
-// stolen from here:
-// https://stackoverflow.com/questions/13462001/ease-in-and-ease-out-animation-formula
-// Implements an ease in speed modulation between 0 and 1
-float SidebarArea::easeIn(float t)
-{
-    if (t <= 0.5f)
-        return 2.0f * t * t;
-    if (t <= 1.0f)
-    {
-        t -= 0.5f;
-        return 2.0f * t * (1.0f - t) + 0.5f;
-    }
-    else
-    {
-        return 1.0;
-    }
-}
 
 void SidebarArea::update()
 {
--- src/UserInterface/Sidebar/SidebarArea.h
+++ src/UserInterface/Sidebar/SidebarArea.h
@@ -44,10 +44,7 @@ class SidebarArea : public juce::AnimatedAppComponent, public TaskListener
     // Component inherited
     bool taskHandler(std::shared_ptr<Task> task) override;
     void paint(juce::Graphics &) override;
-    void paintOverChildren(juce::Graphics &) override;
     void resized() override;
-    // local
-    void notifyMessage(const juce::String &);
     // Timer inherited
     void update() override;
 
@@ -62,36 +59,10 @@ class SidebarArea : public juce::AnimatedAppComponent, public TaskListener
 
   private:
     //==============================================================================
-    // isHidden relates to the supposed position of the animation was finished.
-    // isANimationRUnning tells if the animation is currently running.
-    bool isHidden, isAnimationRunning;
-    std::queue<NotificationMessage> notifQueue;
-    NotificationMessage lastNotification;
-    // the base position of the notif box, base position does not include
-    // outermargins. as we're right aligned it's the widget width minus notif box
-    // width.
-    int notifBaseX, notifBaseY;
-    // the animation destination relative to base position
-    int destinationX, destinationY;
-    // Position of the popup including animation movements relative to base
-    // position.
-    int popupX, popupY;
 
-    // buffer value used in the drawing function
-    int maxWidth;
-
-    // the current notif box positiooutput_addresses_ids[addr]
-    // timers for animations
-    int now, timeSinceAnimStart, animStartTime;
     // size and position of main content widget
     juce::Rectangle<int> bounds;
     juce::Rectangle<int> projectTitleArea;
-    // mutex for list updates.
-    // While list appends can happen in different threads due to notifyError,
-    // other variables are only used by the gui thread and should be safe to rw.
-    juce::ReadWriteLock queueRwMutex;
-    // a value that store total width the animation have to move in
-    float animationNormalisingFactor;
 
     PlayButton playButton;
     StopButton stopButton;
@@ -107,8 +78,6 @@ class SidebarArea : public juce::AnimatedAppComponent, public TaskListener
     ActivityManager &activityManager;
 
     //==============================================================================
-    void trimNotifications();
-    float easeIn(float t);
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SidebarArea)
 };
 
